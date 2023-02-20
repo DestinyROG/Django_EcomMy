b730f40e730caa832dc969e3fd7a1562a592b8fb
DestinyROG <nkundu184@gmail.com>
2023-02-20 10:53:25 +0530

    second commit

--- dbdjangoecom/settings.py
+++ dbdjangoecom/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'dbdjangoecom.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqbpro',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
