50a843d713ba68ae3c74a82912d610e9b80b018a
DestinyROG <nkundu184@gmail.com>
2023-02-20 21:52:51 +0530

    vercel....

--- dbdjangoecom/settings.py
+++ dbdjangoecom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*bk5@b$17%fbop35(71r7#3uv6a7rv_475kpl-nu3bq!brsll3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
