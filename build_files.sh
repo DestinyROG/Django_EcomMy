a79d2222cefc00b85376b683a0982c276b79b2e7
DestinyROG <nkundu184@gmail.com>
2023-02-20 21:57:54 +0530

    vv

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 
 echo "BUILD START"
-python3.11.1 -m pip install -r requirments.txt
-python3.11.1 manage.py collectstatic --noinput --clear
+python3.9 -m pip install -r requirments.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUID END"
