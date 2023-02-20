67a08b5c37e6d0eb15db48a9557f499dca23912e
DestinyROG <nkundu184@gmail.com>
2023-02-20 22:02:43 +0530

    nmn

--- dbdjangoecom/settings.py
+++ dbdjangoecom/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 import os
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR,'Static']
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
+STATIC_ROOT=os.path.join(BASE_DIR,'Static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
